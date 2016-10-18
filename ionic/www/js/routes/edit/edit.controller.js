7956e0ca2a267d6e57105536d2fcca011945ec2d
Luu Nguyen <luunguyen@ccintegration.com>
2016-10-18 22:23:50 +0700

    Update

--- ionic/www/js/routes/edit/edit.controller.js
+++ ionic/www/js/routes/edit/edit.controller.js
@@ -21,6 +21,18 @@
      */
     function EditCtrl($rootScope, user) {
         var vm = this;
+        vm.user = {};
+        vm.edit = function(user, isValid) {
+            if(!isValid) {return;}
+            Authentication.edit(user).then(function () {
+                // save user profile details to $rootScope
+                $rootScope.me = Authentication.getCurrentUser();
+
+                $state.go('app.users', { userId: $rootScope.me._id});
+            }, function(err) {
+                console.error('error' + err);
+            });
+        };
     }
 
     angular
